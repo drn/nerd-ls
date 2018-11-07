90132bda23192f8311e60cba5829fc22248b49e8
Darren Cheng <darren@thanx.com>
2018-11-06 20:20:24 -0800

    Prototype mode colorization.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "flag"
   "strings"
   "strconv"
+  "github.com/fatih/color"
   "github.com/drn/nerd-ls/list"
   "golang.org/x/crypto/ssh/terminal"
 )
@@ -53,7 +54,7 @@ func displayLong(list list.List) {
     )
     fmt.Printf(
       "%s %d %s %s %s%d %s\n",
-      node.Mode,
+      formatMode(node.Mode),
       node.LinkCount,
       node.User,
       node.Group,
@@ -62,6 +63,29 @@ func displayLong(list list.List) {
       node.Name,
     )
   }
+}
+
+func formatMode(mode string) string {
+  runes := []rune(mode)
+
+  return fmt.Sprintf(
+    "%s%s%s%s%s%s%s%s%s%s",
+    colorize(runes[0], color.New(color.FgWhite, color.Bold)),
+    colorize(runes[1], color.New(color.FgGreen)),
+    colorize(runes[2], color.New(color.FgGreen)),
+    colorize(runes[3], color.New(color.FgGreen)),
+    colorize(runes[4], color.New(color.FgYellow)),
+    colorize(runes[5], color.New(color.FgYellow)),
+    colorize(runes[6], color.New(color.FgYellow)),
+    colorize(runes[7], color.New(color.FgRed)),
+    colorize(runes[8], color.New(color.FgRed)),
+    colorize(runes[9], color.New(color.FgRed)),
+  )
+}
+
+func colorize(mode rune, color *color.Color) string {
+  if mode == '-' { return "-" }
+  return color.Sprintf("%c", mode)
 }
 
 func intLength(size int) int {
