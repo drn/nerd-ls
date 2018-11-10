d07c2c67037b5fdb3678fec87ad28bacfc92abeb
Darren Cheng <darren@thanx.com>
2018-11-09 23:31:30 -0800

    Move StripColor() to util package.

---
+++ util/root.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+  "regexp"
+)
+
+const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]" +
+             "*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=>" +
+             "<~]))"
+var ansiRegex = regexp.MustCompile(ansi)
+
+// StripColor - strips ANSI color codes from string
+func StripColor(str string) string {
+  return ansiRegex.ReplaceAllString(str, "")
+}
