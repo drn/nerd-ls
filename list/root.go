f1bcf704eff0c3f3cc68213169ededd3c28a8277
Darren Cheng <darren@thanx.com>
2018-11-09 21:02:40 -0800

    Parallelization prototype.
    Closes https://github.com/drn/nerd-ls/issues/12

--- list/root.go
+++ list/root.go
@@ -3,31 +3,50 @@ package list
 import (
   "os"
   "log"
+  "sync"
   "io/ioutil"
   "github.com/drn/nerd-ls/node"
 )
+
+var waitGroup sync.WaitGroup
+var mutex = sync.RWMutex{}
 
 // Fetch - Fetch List representing current directory
 func Fetch(dir string, options map[string]bool) []node.Node {
   files, err := ioutil.ReadDir(dir)
   if err != nil { log.Fatal(err) }
 
-  nodes := make([]node.Node, len(files)+2)
+  length := len(files)
+  if options["all"] { length += 2 }
+
+  nodes := make([]node.Node, length)
 
   index := 0
   if options["all"] {
+
     file, _ := os.Stat(".")
     nodes[0] = node.New(file)
+
     file, _ = os.Stat("..")
     nodes[1] = node.New(file)
+
     index += 2
   }
 
   for i:=0; i<len(files); i++ {
     if !options["all"] && []rune(files[i].Name())[0] == '.' { continue }
-    nodes[index] = node.New(files[i])
+
+    waitGroup.Add(1)
+    go func(file os.FileInfo, index int) {
+      defer waitGroup.Done()
+      mutex.Lock()
+      nodes[index] = node.New(file)
+      mutex.Unlock()
+    }(files[i], index)
+
     index++
   }
+  waitGroup.Wait()
 
   return nodes[:index]
 }
