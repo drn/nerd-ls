3f470d330721f1234c9d082dce21ddd98c69c57a
Darren Cheng <darren@thanx.com>
2018-11-27 23:31:19 -1000

    Rename nodeColor regex and support ignore regex.

--- format/root.go
+++ format/root.go
@@ -6,11 +6,13 @@ import (
   "github.com/drn/nerd-ls/node"
 )
 
-var origRegex = regexp.MustCompile(`.*\.orig$`)
+var errorRegex = regexp.MustCompile(`.*\.orig$`)
+var ignoreRegex = regexp.MustCompile(`^.DS_Store$`)
 
 func nodeColor(node node.Node) *color.Color {
   if node.Symlink != "" { return color.New(color.FgMagenta) }
   if node.IsDir { return color.New(color.FgCyan, color.Bold) }
-  if origRegex.MatchString(node.Name) { return color.New(color.FgRed) }
+  if ignoreRegex.MatchString(node.Name) { return color.New(color.FgBlack, color.Bold) }
+  if errorRegex.MatchString(node.Name) { return color.New(color.FgRed) }
   return color.New(color.FgWhite)
 }
