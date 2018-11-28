5d78dbc2be75d4a60ee2bcc0eff9b972964ed28a
Darren Cheng <darren@thanx.com>
2018-11-27 23:24:27 -1000

    Mark *.orig files in red by default.
    Resolves https://github.com/drn/nerd-ls/issues/6

--- format/root.go
+++ format/root.go
@@ -1,12 +1,16 @@
 package format
 
 import (
+  "regexp"
   "github.com/fatih/color"
   "github.com/drn/nerd-ls/node"
 )
 
+var origRegex = regexp.MustCompile(`.*\.orig$`)
+
 func nodeColor(node node.Node) *color.Color {
   if node.Symlink != "" { return color.New(color.FgMagenta) }
-  if !node.IsDir { return color.New(color.FgWhite) }
-  return color.New(color.FgCyan, color.Bold)
+  if node.IsDir { return color.New(color.FgCyan, color.Bold) }
+  if origRegex.MatchString(node.Name) { return color.New(color.FgRed) }
+  return color.New(color.FgWhite)
 }
